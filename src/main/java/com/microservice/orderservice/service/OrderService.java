4a82501e4144b97ce53192f01239f0a13c7bd717
rana Aditya <ranarajput9548@gmail.com>
2024-10-15 23:02:27 +0530

    order service created

---
+++ src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -0,0 +1,29 @@
+package com.microservice.orderservice.service;
+
+import com.microservice.orderservice.dto.OrderRequest;
+import com.microservice.orderservice.model.Order;
+import com.microservice.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+//    private final InventoryClient inventoryClient;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+
+    }
+}
