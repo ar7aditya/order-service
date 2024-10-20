79feb2453f6a842a25844212595ac8bf80a551c9
rana Aditya <ranarajput9548@gmail.com>
2024-10-21 02:46:28 +0530

    made inter-service communication

--- src/main/java/com/microservice/orderservice/service/OrderService.java
+++ src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservice.orderservice.service;
 
+import com.microservice.orderservice.client.InventoryClient;
 import com.microservice.orderservice.dto.OrderRequest;
 import com.microservice.orderservice.model.Order;
 import com.microservice.orderservice.repository.OrderRepository;
@@ -15,15 +16,23 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-//    private final InventoryClient inventoryClient;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        boolean isProductIsInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductIsInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }
+        else{
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + "is not in stock");
+        }
 
     }
 }
--- src/test/java/com/microservice/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/microservice/orderservice/OrderServiceApplicationTests.java
@@ -1,15 +1,87 @@
 package com.microservice.orderservice;
 
+import com.microservice.orderservice.stubs.InventoryClientStub;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.shaded.org.hamcrest.Matcher;
+import org.testcontainers.shaded.org.hamcrest.Matchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void shouldSubmitOrder() {
+        String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+        InventoryClientStub.stubInventoryCall("iphone_15", 1);
+
+        var responseBodyString = RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().asString();
+
+        assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+    }
+
+    private void assertThat(String responseBodyString, Matcher<String> order_placed_successfully) {
+    }
+
+    @Test
+    void shouldFailOrderWhenProductIsNotInStock() {
+        String submitOrderJson = """
+                {
+                     "skuCode": "iphone-15",
+                     "price": 1000,
+                     "quantity": 1000
+                }
+                """;
+        InventoryClientStub.stubInventoryCall("iphone-15", 1000);
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(500);
     }
 
 }
